52b239647b7662c5e8c55401311d300e43b6d1c2
jabed366 <jabedhasan231@gmail.com>
2022-04-15 16:05:11 +0600

    First Commit

--- src/Component/practice2.c
+++ src/Component/practice2.c
@@ -13,16 +13,22 @@ void electionResult()
     if(strcmp(username,"Solaiman")==0 && strcmp(password,"123")==0){
         printf("\nWelcome.Login Success!");
         int wonByVote;
-        if (Shahin > Lubaba && Shahin > Joy && Shahin > Johir)
+        if(Shahin == Lubaba && Shahin == Joy && Shahin == Johir)
         {
-        printf("\n***Shahin won the election***\n\n");
-        printf("Total vote of Shahin : %d\n",Shahin);
-        wonByVote=Shahin-Lubaba;
-        printf("Shahin won by %d votes to Lubaba\n",wonByVote);
-        wonByVote=Shahin-Joy;
-        printf("Shahin won by %d votes to Joy\n",wonByVote);
-        wonByVote=Shahin-Johir;
-        printf("Shahin won by %d votes to Johir\n",wonByVote);
+            printf("\nNo one won the election\n\n");
+            printf("            Shahin---Lubaba---Joy---Johir\n");
+            printf("Total Vote   %d       %d        %d     %d\n",Shahin,Lubaba,Joy,Johir);
+        }
+        else if (Shahin > Lubaba && Shahin > Joy && Shahin > Johir)
+        {
+            printf("\n***Shahin won the election***\n\n");
+            printf("Total vote of Shahin : %d\n",Shahin);
+            wonByVote=Shahin-Lubaba;
+            printf("Shahin won by %d votes to Lubaba\n",wonByVote);
+            wonByVote=Shahin-Joy;
+            printf("Shahin won by %d votes to Joy\n",wonByVote);
+            wonByVote=Shahin-Johir;
+            printf("Shahin won by %d votes to Johir\n",wonByVote);
         }
         else if (Lubaba > Joy && Lubaba > Johir)
         {
@@ -35,7 +41,7 @@ void electionResult()
             wonByVote=Lubaba-Johir;
             printf("Lubaba won by %d votes to Johir\n",wonByVote);
         }
-        if (Joy > Johir)
+        else if (Joy > Johir)
         {
             printf("\n***Joy won the election***\n\n");
             printf("Total vote of Joy : %d\n",Joy);
@@ -45,12 +51,6 @@ void electionResult()
             printf("Joy won by %d votes to Shahin\n",wonByVote);
             wonByVote=Joy-Johir;
             printf("Joy won by %d votes to Johir\n",wonByVote);
-        }
-        else if(Shahin == Lubaba && Shahin == Joy && Shahin == Johir)
-        {
-            printf("\nNo one won the election\n\n");
-            printf("            Shahin---Lubaba---Joy---Johir\n");
-            printf("Total Vote   %d       %d        %d     %d\n",Shahin,Lubaba,Joy,Johir);
         }
         else
         {
@@ -65,7 +65,7 @@ void electionResult()
         }  
     }
     else{ 
-    printf("\nUser doesn't exist"); 
+    printf("\nInvalid User Login"); 
     } 
     
 }
@@ -102,9 +102,6 @@ int main()
         printf("*************************************************************\n\n");
 
 
-
-        
-
     do
     {    
         printf("Press 1 to vote Shahin\n");
@@ -114,7 +111,6 @@ int main()
         printf("Press 5 to show election result\n");
         printf("Please choose : ");
         scanf("%d", &choose);
-
         if(choose==1 || choose==2 || choose==3 || choose==4){
             system ("cls");  
         }
